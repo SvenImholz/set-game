c85285db2834432c196ea2e31adcfc1099898654
GreyHunt3r <svenimholz@icloud.com>
2024-04-19 14:30:18 +0200

    Deleted unused stuff

--- Set.Application/Common/Errors/DuplicateEmailException.cs
+++
@@ -1,9 +0,0 @@
-using System.Net;
-
-namespace Set.Application.Common.Errors;
-
-public class DuplicateEmailException : Exception, IServiceException
-{
-    public HttpStatusCode StatusCode => HttpStatusCode.Conflict;
-    public string ErrorMessage => "Email already exists";
-}
