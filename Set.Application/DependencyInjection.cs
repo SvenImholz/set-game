964a761830020473c6c1d7e8a480769b339ff60b
GreyHunt3r <svenimholz@icloud.com>
2024-04-19 17:27:38 +0200

    Add MediatR to DependencyInjection.cs

--- Set.Application/DependencyInjection.cs
+++ Set.Application/DependencyInjection.cs
@@ -1,5 +1,5 @@
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using Set.Application.Services.Authentication;
 
 namespace Set.Application;
 
@@ -7,7 +7,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
 
         return services;
     }
