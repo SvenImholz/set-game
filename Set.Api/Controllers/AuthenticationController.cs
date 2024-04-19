ec447c14ba8677843a294e9054655e0b243639e3
GreyHunt3r <svenimholz@icloud.com>
2024-04-19 17:28:58 +0200

    Implemented MediatR to Authentication Controller

--- Set.Api/Controllers/AuthenticationController.cs
+++ Set.Api/Controllers/AuthenticationController.cs
@@ -1,50 +1,61 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Set.Api.Filters;
-using Set.Application.Services.Authentication;
+using Set.Application.Authentication.Commands.Register;
+using Set.Application.Authentication.Common;
+using Set.Application.Authentication.Queries.Login;
 using Set.Contracts.Authentication;
+using Set.Domain.Common.Errors;
 
 namespace Set.Api.Controllers;
 
-[ApiController]
 [Route("api/auth")]
-public class AuthenticationController : ControllerBase
+public class AuthenticationController : ApiController
 {
-    readonly IAuthenticationService _authenticationService;
+    readonly ISender _sender;
 
-    public AuthenticationController(IAuthenticationService authenticationService) { _authenticationService = authenticationService; }
+    public AuthenticationController(ISender sender) { _sender = sender; }
 
     [HttpPost("register")]
-    public IActionResult Register(RegisterRequest request)
+    public async Task<IActionResult> Register(RegisterRequest request)
     {
-        var authResult =
-            _authenticationService.Register(
+        var command = new RegisterCommand(
             request.FirstName,
             request.LastName,
             request.Email,
             request.Password);
+        
+        var authResult = await _sender.Send(command);
 
-        var response = new AuthenticationResponse(
-        authResult.User.Id,
-        authResult.User.FirstName,
-        authResult.User.LastName,
-        authResult.User.Email,
-        authResult.Token);
+        return authResult.Match(
+        authResult => Ok(MapAuthResponse(authResult)),
+        Problem);
 
-        return Ok(response);
     }
-
+    
     [HttpPost("login")]
-    public IActionResult Login(LoginRequest request)
+    public async Task<IActionResult> Login(LoginRequest request)
     {
-        var authResult = _authenticationService.Login(request.Email, request.Password);
+        var query = new LoginQuery(
+            request.Email,
+            request.Password);
+        var authResult = await _sender.Send(query);
 
-        var response = new AuthenticationResponse(
+        if (authResult.IsError && authResult.FirstError == Errors.Authentication.InvalidCredentials)
+            return Problem(statusCode: StatusCodes.Status401Unauthorized, title: authResult.FirstError.Description);
+
+        return authResult.Match(
+        authResult => Ok(MapAuthResponse(authResult)),
+        Problem);
+    }
+
+    private AuthenticationResponse MapAuthResponse(AuthenticationResult authResult)
+    {
+        return new AuthenticationResponse(
         authResult.User.Id,
         authResult.User.FirstName,
         authResult.User.LastName,
         authResult.User.Email,
         authResult.Token);
-
-        return Ok(response);
     }
+
 }
