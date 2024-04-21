c6e4a5ec596d1a4a8f9fd65d2ef6a491495536bf
GreyHunt3r <svenimholz@icloud.com>
2024-04-21 15:05:49 +0200

    Add validation for loginQuery

---
+++ Set.Application/Authentication/Queries/Login/LoginQueryValidator.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+
+namespace Set.Application.Authentication.Queries.Login;
+
+public class LoginQueryValidator : AbstractValidator<LoginQuery>
+{
+    public LoginQueryValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(6);
+    }
+}
