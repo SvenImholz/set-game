8315dd1c3f15cde94551b68ca09f384b0050ef6f
GreyHunt3r <svenimholz@icloud.com>
2024-06-02 14:41:05 +0200

    Add Repository interface

--- Set.Application/Common/Interfaces/Persistence/IGameRepository.cs
+++ Set.Application/Common/Interfaces/Persistence/IGameRepository.cs
@@ -1,6 +1,9 @@
+using Set.Domain.GameAggregate;
+
 namespace Set.Application.Common.Interfaces.Persistence;
 
 public interface IGameRepository
 {
-    //todo: https://www.youtube.com/watch?v=jm0CWlb5vvQ&t=858s follow the tutorial
+    void Add(Game game);
+
 }
