092ac87eccb3cf3d6a3548094051f1c01c3e16b6
GreyHunt3r <svenimholz@icloud.com>
2024-06-02 14:34:06 +0200

    change mapping

--- Set.Api/Common/Mapping/GameMappingConfig.cs
+++ Set.Api/Common/Mapping/GameMappingConfig.cs
@@ -1,15 +1,13 @@
 using Mapster;
-
 using Set.Application.Games.Commands.CreateGame;
-using Set.Contracts.Games;
 
 namespace Set.Api.Common.Mapping;
 
 public class GameMappingConfig : IRegister
 {
-
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<CreateGameRequest, CreateGameCommand>();
+        config.NewConfig<Guid, CreateGameCommand>()
+            .Map(dest => dest.PlayerId, src => src);
     }
 }
