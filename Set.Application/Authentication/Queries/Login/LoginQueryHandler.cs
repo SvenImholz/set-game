23631eba391df44559dc1714854890d4f2774739
GreyHunt3r <svenimholz@icloud.com>
2024-06-02 14:40:16 +0200

    Rename

--- Set.Application/Authentication/Queries/Login/LoginQueryHandler.cs
+++ Set.Application/Authentication/Queries/Login/LoginQueryHandler.cs
@@ -10,30 +10,30 @@ namespace Set.Application.Authentication.Queries.Login;
 public class LoginQueryHandler : IRequestHandler<LoginQuery, ErrorOr<AuthenticationResult>>
 {
     readonly IJwtTokenGenerator _jwtTokenGenerator;
-    readonly IUserRepository _userRepository;
-    
-    public LoginQueryHandler(IUserRepository userRepository, IJwtTokenGenerator jwtTokenGenerator)
+    readonly IPlayerRepository _playerRepository;
+
+    public LoginQueryHandler(IPlayerRepository playerRepository, IJwtTokenGenerator jwtTokenGenerator)
     {
-        _userRepository = userRepository;
+        _playerRepository = playerRepository;
         _jwtTokenGenerator = jwtTokenGenerator;
     }
 
     public async Task<ErrorOr<AuthenticationResult>> Handle(LoginQuery query, CancellationToken cancellationToken)
     {
         await Task.CompletedTask;
-        // Validate that the user exists
-        if (_userRepository.GetUserByEmail(query.Email) is not {} user)
+        // Validate that the player exists
+        if (_playerRepository.GetPlayerByEmail(query.Email) is not {} player)
             return Errors.Authentication.InvalidCredentials;
 
         // Validate that the password is correct
-        if (user.Password != query.Password)
+        if (player.Password != query.Password)
             return Errors.Authentication.InvalidCredentials;
-        
+
         // Create JWT Token
-        var token = _jwtTokenGenerator.GenerateToken(user);
+        var token = _jwtTokenGenerator.GenerateToken(player);
 
         return new AuthenticationResult(
-        user,
+        player,
         token);
     }
 }
