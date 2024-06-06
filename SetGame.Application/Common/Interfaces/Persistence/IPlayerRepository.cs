41271d4cacee61428b114e5e0a8aec151a569993
GreyHunt3r <svenimholz@icloud.com>
2024-06-06 14:12:15 +0200

    Add GetPlayerById to PlayerRepository

--- SetGame.Application/Common/Interfaces/Persistence/IPlayerRepository.cs
+++ SetGame.Application/Common/Interfaces/Persistence/IPlayerRepository.cs
@@ -5,5 +5,6 @@ namespace SetGame.Application.Common.Interfaces.Persistence;
 public interface IPlayerRepository
 {
     Player? GetPlayerByEmail(string email);
+    Player? GetPlayerById(Guid playerId);
     void Add(Player player);
 }
